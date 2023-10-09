19f4676d7a5095383ae5803814d5352b5ffdcfca
dev25082 <devkhandelwal25082002@gmail.com>
2023-10-09 15:58:11 +0530

    Javascript file added

---
+++ 01.js
@@ -0,0 +1,8 @@
+//  ctrl+/--> comment
+
+// console.log can print something on console
+
+// /* hello world program */
+console.log("hello world");
+console.log('hello world');
+console.log(`hello world`);

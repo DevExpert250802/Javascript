6e51ec7ff2ec55dabd3e00dfeecbc2dc797d2ab8
Devansh Khandelwal <108886695+dev25082@users.noreply.github.com>
2023-10-25 23:35:04 +0530

    Add files via upload

---
+++ 10.js
@@ -0,0 +1,10 @@
+//  template string
+let age = 22;
+let firstName = "devansh";
+
+//"my name is devansh and my age is 22"
+// let aboutMe = "my name is " + firstName + " and my age is " + age;
+//  console.log(aboutMe);
+ 
+let aboutMe = `my name is ${firstName} and my age is ${age} `
+console.log(aboutMe);

80bc09059a8fe08ffdd51ea309c0b13305ec9ac3
Devansh Khandelwal <108886695+dev25082@users.noreply.github.com>
2023-11-15 22:58:22 +0530

    Add files via upload

---
+++ 25.js
@@ -0,0 +1,15 @@
+// do while loop
+// generally used to execute false condition at a time
+
+// let i = 10;
+// while(i<=9){
+//     console.log(i);
+//     i++;
+// }
+
+  let i = 10;
+do{
+    console.log(i);
+    i++;
+}while(i<=9);
+console.log("value of i is ", i)

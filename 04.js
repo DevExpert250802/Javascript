8f6b7efe1750cc313c8d70fd3fb42f49ce90da2e
Devansh Khandelwal <108886695+dev25082@users.noreply.github.com>
2023-11-23 13:38:52 +0530

    Add files via upload

--- 04.js
+++ 04.js
@@ -2,14 +2,19 @@
 
 // declare variable with let keyword 
 
+
 // var firstName = "devansh";
 // var firstName = "devansh";
+
+
+// let firstName = "devansh";
+// let firstName = "dev"; (error)
 
 let firstName = "devansh";
 firstName = "dev"; // (valid)
 
-// let firstName= "dev";(error) (we can declare new variable in case of var using var 2 times but we can not declare new variable with using let 2 times)
+ // let firstName= "dev";(error) (we can declare new variable in case of var using var 2 times but we can not declare new variable with using let 2 times)
 
 console.log(firstName);
 
-// block scope vs function scope
+ // block scope vs function scope

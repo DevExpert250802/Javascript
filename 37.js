477905fd8d951b00bc18eab0dff8fcc218cc2655
Devansh Khandelwal <108886695+dev25082@users.noreply.github.com>
2023-11-28 20:41:10 +0530

    Add files via upload

---
+++ 37.js
@@ -0,0 +1,17 @@
+// difference between dot and bracket Notation
+
+// const key = "email"
+;
+// const person = {
+//     name: "harshit",
+//     age: 22,
+//     "person hobbies": ["guitar", "sleeping", "listening music"] ( key with more than 2 word) )
+
+// }
+// console.log(person.person hobbies);// error due to space
+// console.log(person["person hobbies"]);
+
+//// person.key = "devansh@gmail.com"; // key hee key ban gayi
+
+// person[key] = "harshitvashisth@gmail.com";// key is email
+// console.log(person); 

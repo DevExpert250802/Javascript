561b8c353c51c51ebf43a1b2161bc661a09707d8
dev25082 <devkhandelwal25082002@gmail.com>
2023-12-12 00:30:53 +0530

    Your commit

--- 31.js
+++
@@ -1,9 +0,0 @@
-// use const for creating Array or any refrence types (as used by 90% developers)
-
-// const pi = 3.14;
-// pi = 4.7  (error as const can not be re-defined)
-// array stored in heap memory fruits = ["apples","mango"]; // 0x11
-const fruits = ["apples","mango"]; // 0x11
-// fruits=["grapes","pineapple"] (error)
-fruits.push("banana");
-console.log(fruits);

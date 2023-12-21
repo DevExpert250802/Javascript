6765f33c45372d3f8ebcf0abf67d31f47ee5925a
Devansh Khandelwal <108886695+dev25082@users.noreply.github.com>
2023-12-21 05:57:56 +0530

    Add files via upload

---
+++ 53.js
@@ -0,0 +1,25 @@
+// parameter destructuring 
+  
+
+// object 
+// react 
+
+const person = {
+    firstName: "harshit",
+    gender: "male",  
+    age: 500
+}
+
+// function printDetails(obj){
+//     console.log(obj.firstName);
+//     console.log(obj.gender);
+//  console.log(obj.gender);
+// }
+
+function printDetails({firstName, gender, age}){
+    console.log(firstName);
+    console.log(gender);
+    console.log(age);
+}
+
+printDetails(person);

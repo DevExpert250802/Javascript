08a502957b166efa5364ebb8cf8dcdb6400d13bc
Devansh Khandelwal <108886695+dev25082@users.noreply.github.com>
2024-01-04 22:12:35 +0530

    Add files via upload

---
+++ 69.js
@@ -0,0 +1,18 @@
+// clone using Object.assign 
+
+//  heap memory  
+
+const obj = {
+    key1: "value1",
+    key2: "value2"
+}
+//// const obj2 = {...obj};
+//// const obj2 = Object.assign({}, obj);
+
+// const obj2 = {'key69': "value69",...obj};
+// const obj2 = Object.assign({'key69': "value69"}, obj);
+
+
+// obj.key3 = "value3";
+// console.log(obj);
+// console.log(obj2);

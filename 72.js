bc2d86b96111377cd13052ef8cf96c3f5565b56d
Devansh Khandelwal <108886695+dev25082@users.noreply.github.com>
2024-01-08 21:28:19 +0530

    Add files via upload

---
+++ 72.js
@@ -0,0 +1,18 @@
+// window Object 
+
+// window Object -> global Object
+
+//     console.log(this);// window Object 
+// console.log(window);// window Object 
+
+
+// function myFunc(){
+//     "use strict";
+//     console.log(this);
+// }
+// myFunc();
+
+// function myFunc(){
+//     console.log("hello world");
+// }
+// myFunc();

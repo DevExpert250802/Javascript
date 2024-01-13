fb53c6724f9b8f0f3fa922dbb0c4ea8d27aecc8b
Devansh Khandelwal <108886695+dev25082@users.noreply.github.com>
2024-01-13 22:12:10 +0530

    Add files via upload

---
+++ 76.js
@@ -0,0 +1,20 @@
+// short Syntax
+
+// const user1 = {
+//     firstName : "harshit",
+//     age: 8,
+//     about: function(){
+//         console.log(this.firstName, this.age);
+//     }   
+// }
+
+// const user1 = {
+//     firstName : "harshit",
+//     age: 8,
+//     about(){
+//         console.log(this.firstName, this.age); //about method
+//     }   
+// }
+
+
+user1.about();

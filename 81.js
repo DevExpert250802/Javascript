82bbb682565770ffc5fcfbf15be20895e62f3cea
Devansh Khandelwal <108886695+dev25082@users.noreply.github.com>
2024-01-20 06:14:40 +0530

    Add files via upload

---
+++ 81.js
@@ -0,0 +1,31 @@
+//prototype
+
+function hello(){
+    console.log("hello world");
+}
+
+// javascript function ===> function  + object
+
+// console.log(hello.name);
+
+// you can add your own properties 
+// hello.myOwnProperty = "very unique value";
+// console.log(hello.myOwnProperty);
+
+// name property ---> tells function name;
+
+// function provides more usefull properties.
+// function provides (empty object {}) => prototype .
+
+
+// console.log(hello.prototype); // {}
+
+// only functions provide prototype property
+
+hello.prototype.abc = "abc";
+hello.prototype.xyz = "xyz";
+hello.prototype.sing = function(){
+    return "lalalla";
+};
+console.log(hello.prototype);
+console.log(hello.prototype.sing());

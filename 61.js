a59639858bbbf28038ba33712d6a040a7340d636
Devansh Khandelwal <108886695+DevExpert250802@users.noreply.github.com>
2024-10-05 21:15:25 +0530

    Update 61.js

--- 61.js
+++ 61.js
@@ -2,22 +2,35 @@
 
 // const myArrary = ["Hello", "cat", "dog","lion" ];
 
-// example 1
-// function isLength(string){
-//     return string.length === 3;
-// }
- //  myArray.find()
+/*  example 1
 
-//  const ans = isLength("doog");
-// console.log(ans);
 
-// const ans = myArrary.find(isLength);
-// console.log(ans);
+
+function isLength(string){
+    return string.length === 3;
+}
+  myArray.find()
+
+ const ans = isLength("doog");
+console.log(ans);
+
+const ans = myArrary.find(isLength);
+console.log(ans);
  
+const ans = myArrary.find((string)=>string.length===3);
+console.log(ans);
+
+
+*/
+
+
+
+
 // example 2
-// const ans = myArrary.find((string)=>string.length===3);
-// console.log(ans);
-  
+
+
+
+
 const users = [
     {userID : 1, userName: "harshit"},
     {userID : 2, userName: "devansh"},
@@ -26,9 +39,9 @@ const users = [
     {userID : 5, userName: "harshig"},
 ]; 
 
-const myUser = users.find((user)=>{
-    return user.userID===3
-});
+// const myUser = users.find((user)=>{
+//     return user.userID===3
+// });
 
-// const myUser = users.find((user)=>user.userID===3);
+const myUser = users.find( user => user.userID === 3);
 console.log(myUser);

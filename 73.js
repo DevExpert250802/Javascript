daa34a237953685ec366d29a6ee6e7263d9ab4b6
Devansh Khandelwal <108886695+DevExpert250802@users.noreply.github.com>
2024-10-07 11:30:09 +0530

    Update 73.js

--- 73.js
+++ 73.js
@@ -1,33 +1,48 @@
 // call , apply , bind methods
 
-// const user1 = {
-//     firstName : "harshit",
-//     age: 8,   
-//     about : function (){
-//         console.log(this.firstName, this.age);
-//     }
-// }
 
-// const  user2 = {
-//     firstName : "mohit", 
-//     age: 9,
-// }
+/*
 
-// user1.about.call(user2) // mohit     9
-// user1.about.call()     // undefined  undefined
-// user1.about.call(user1) // harshit   8
+const user1 = {
+    firstName : "harshit",
+    age: 8,   
+    about : function (){
+        console.log(this.firstName, this.age);
+    }
+}
 
-// const user1 = {
-//     firstName : "harshit",
-//     age: 8,   
-//     about : function (hobby,favMusician){
-//         console.log(this.firstName, this.age,hobby,favMusician);
-//     }
-// }
-// user1.about.call(user2,"guitar","mojart") // mohit 9 guitar mozart
+const  user2 = {
+    firstName : "mohit", 
+    age: 9,
+}
 
+user1.about.call(user2) // mohit     9
+user1.about.call()     // undefined  undefined
+user1.about.call(user1) // harshit   8
 
-// defining function outside object
+*/
+
+/*
+
+const user1 = {
+    firstName : "harshit",
+    age: 8,   
+    about : function (hobby,favMusician){
+        console.log(this.firstName, this.age,hobby,favMusician);
+    }
+}
+
+const  user2 = {
+    firstName : "mohit", 
+    age: 9,
+}
+user1.about.call(user2,"guitar","mojart") // mohit 9 guitar mozart
+
+*/
+
+ // defining function outside object
+
+ 
 function about(hobby, favMusician){
     console.log(this.firstName, this.age, hobby, favMusician);
 }
@@ -49,12 +64,13 @@ about.call(user1,"guitar","mojart")
 about.call(user2,"guitar","mojart")
 
 
-// apply
-//array pass
+
+// // apply
+// //array pass
 
 about.apply(user1, ["guitar", "bach"]);
 
-// bind
+// // bind
 // function return
 about.bind(user1, ["guitar", "bach"]); // nothing print
 
